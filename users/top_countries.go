836cc7a3151a04aa383818c63ee37af021020228
Fedor I.Kryukov <f.kryukov@codemaster.pro>
2022-05-04 18:36:09 +0300

    top countries db methods

---
+++ users/top_countries.go
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+package users
+
+import (
+	"context"
+
+	"github.com/framey-io/go-tarantool"
+	"github.com/pkg/errors"
+)
+
+func (u *users) topCountriesInc(ctx context.Context, countryNew string) error {
+	if ctx.Err() != nil {
+		return errors.Wrap(ctx.Err(), "inc countries failed because context failed")
+	}
+
+	return errors.Wrapf(u.db.UpdateTyped("USERS_PER_COUNTRY", "pk_unnamed_USERS_PER_COUNTRY_1",
+		tarantool.StringKey{S: countryNew}, []tarantool.Op{{Op: "+", Field: 1, Arg: 1}}, &[]*user{}),
+		"error updating USERS_PER_COUNTRY")
+}
+
+func (u *users) topCountriesDec(ctx context.Context, countryOld string) error {
+	if ctx.Err() != nil {
+		return errors.Wrap(ctx.Err(), "dec countries failed because context failed")
+	}
+
+	return errors.Wrapf(u.db.UpdateTyped("USERS_PER_COUNTRY", "pk_unnamed_USERS_PER_COUNTRY_1",
+		tarantool.StringKey{S: countryOld}, []tarantool.Op{{Op: "-", Field: 1, Arg: 1}}, &[]*user{}),
+		"error updating USERS_PER_COUNTRY")
+}
+
+func (u *users) TopCountriesGet(ctx context.Context, limit, offset uint64) (cs []*CountryStatistics, err error) {
+	if ctx.Err() != nil {
+		return nil, errors.Wrap(ctx.Err(), "get top countries failed because context failed")
+	}
+
+	var result []*CountryStatistics
+
+	sql := `SELECT country, user_count FROM users_per_country ORDER BY user_count desc LIMIT :limit OFFSET :offset`
+	params := map[string]interface{}{"limit": limit, "offset": offset}
+
+	if err = u.db.PrepareExecuteTyped(sql, params, &result); err != nil {
+		return nil, errors.Wrap(err, "get top countries failed")
+	}
+
+	return result, nil
+}
