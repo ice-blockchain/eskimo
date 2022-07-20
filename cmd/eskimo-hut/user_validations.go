737d70a6e8757102ec4dd4099fb013a740957835
Anton Haurychenka <anton.haurychenka@framey.io>
2022-07-20 09:55:31 +0300

    Swagger update.

--- cmd/eskimo-hut/user_validations.go
+++ cmd/eskimo-hut/user_validations.go
@@ -20,22 +20,22 @@ func (s *service) setupUserValidationRoutes(router *gin.Engine) {
 
 // ValidatePhoneNumber godoc
 // @Schemes
-// @Description  Validates a provided phone number by a one time code previously provided to the user via SMS.
-// @Tags         Validations
-// @Accept       json
-// @Produce      json
-// @Param        Authorization  header  string                       true  "Insert your access token"  default(Bearer <Add access token here>)
-// @Param        userId         path    string                       true  "ID of the user"
-// @Param        request        body    users.PhoneNumberValidation  true  "Request params"
-// @Success      200            "ok"
-// @Failure      400            {object}  server.ErrorResponse  "if validations fail"
-// @Failure      401            {object}  server.ErrorResponse  "if not authorized"
-// @Failure      403            {object}  server.ErrorResponse  "if not allowed"
-// @Failure      404            {object}  server.ErrorResponse  "phone number is not in the process of validation or user not found"
-// @Failure      422            {object}  server.ErrorResponse  "if syntax fails"
-// @Failure      500            {object}  server.ErrorResponse
-// @Failure      504            {object}  server.ErrorResponse  "if request times out"
-// @Router       /user-validations/{userId}/phone-number [PUT].
+// @Description Validates a provided phone number by a one time code previously provided to the user via SMS.
+// @Tags        Validations
+// @Accept      json
+// @Produce     json
+// @Param       Authorization header string                      true "Insert your access token" default(Bearer <Add access token here>)
+// @Param       userId        path   string                      true "ID of the user"
+// @Param       request       body   users.PhoneNumberValidation true "Request params"
+// @Success     200           "ok"
+// @Failure     400           {object} server.ErrorResponse "if validations fail"
+// @Failure     401           {object} server.ErrorResponse "if not authorized"
+// @Failure     403           {object} server.ErrorResponse "if not allowed"
+// @Failure     404           {object} server.ErrorResponse "phone number is not in the process of validation or user not found"
+// @Failure     422           {object} server.ErrorResponse "if syntax fails"
+// @Failure     500           {object} server.ErrorResponse
+// @Failure     504           {object} server.ErrorResponse "if request times out"
+// @Router      /user-validations/{userId}/phone-number [PUT].
 func (s *service) ValidatePhoneNumber(ctx context.Context, req *RequestValidatePhoneNumber) server.Response {
 	if err := s.usersProcessor.ValidatePhoneNumber(ctx, &req.PhoneNumberValidation); err != nil {
 		err = errors.Wrapf(err, "validate phone number failed for %#v", &req.PhoneNumberValidation)
