021ed0969d5ded53de9ff9e3f4c7c07e2b8dfba9
ice-myles <96409608+ice-myles@users.noreply.github.com>
2023-04-05 15:01:14 +0300

    Create and delete were migrated to postgres. Fixed some lints.

--- users/contract.go
+++ users/contract.go
@@ -106,7 +106,7 @@ type (
 		MiningBlockchainAccountAddress string `json:"miningBlockchainAccountAddress,omitempty" example:"0x4B73C58370AEfcEf86A6021afCDe5673511376B2"`
 		BlockchainAccountAddress       string `json:"blockchainAccountAddress,omitempty" example:"0x4B73C58370AEfcEf86A6021afCDe5673511376B2"`
 		Language                       string `json:"language,omitempty" example:"en"`
-		HashCode                       uint64 `json:"hashCode,omitempty" example:"43453546464576547" swaggerignore:"true"`
+		HashCode                       int64  `json:"hashCode,omitempty" example:"43453546464576547" swaggerignore:"true"`
 	}
 	MinimalUserProfile struct {
 		Active *NotExpired `json:"active,omitempty" example:"true"`
