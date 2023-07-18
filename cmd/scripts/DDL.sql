2608c33e985a8aa2b036d342c985c87059e4fce2
ice-myles <96409608+ice-myles@users.noreply.github.com>
2023-07-18 16:24:30 +0300

    Deleted obsolete files.

--- cmd/scripts/DDL.sql
+++
@@ -1,9 +0,0 @@
--- SPDX-License-Identifier: ice License 1.0
-CREATE TABLE IF NOT EXISTS update_email_temporary (
-                    phone_number text primary key,
-                    email text NOT NULL UNIQUE);
--- Insert here the data that need to be handled.
-INSERT INTO update_email_temporary (phone_number,email)
-          VALUES
-               ('test', 'test@test.com')
-ON CONFLICT DO NOTHING;
