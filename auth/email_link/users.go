80d7cd2407fc0ec11963e47fb355db571c4b2232
ice-myles <96409608+ice-myles@users.noreply.github.com>
2023-08-08 10:12:12 +0300

    Fixed postgres scanning: added coalesce on getting email link sign ins information on replication lag.

--- auth/email_link/users.go
+++ auth/email_link/users.go
@@ -87,10 +87,10 @@ func (c *client) getUserByIDOrPk(ctx context.Context, userID string, id *loginID
 	usr, err := storage.Get[emailLinkSignIn](ctx, c.db, `
 		SELECT 		created_at,
 					token_issued_at,
-					previously_issued_token_seq, 
-					issued_token_seq,
+					COALESCE(previously_issued_token_seq, 0) 			AS previously_issued_token_seq, 
+					COALESCE(issued_token_seq, 0) 			 			AS issued_token_seq,
 					blocked_until,
-					confirmation_code_wrong_attempts_count,
+					COALESCE(confirmation_code_wrong_attempts_count, 0) AS confirmation_code_wrong_attempts_count,
 					otp,
 					confirmation_code,
 					user_id,
@@ -124,7 +124,7 @@ func (c *client) getUserByIDOrPk(ctx context.Context, userID string, id *loginID
 			SELECT
 					emails.created_at                                  AS created_at,
 					emails.token_issued_at       			 	  	   AS token_issued_at,
-		    		emails.previously_issued_token_seq                         AS previously_issued_token_seq,
+		    		emails.previously_issued_token_seq                 AS previously_issued_token_seq,
 					emails.issued_token_seq       			 	  	   AS issued_token_seq,
 					emails.blocked_until       			 	  	   	   AS blocked_until,
 					emails.confirmation_code_wrong_attempts_count 	   AS confirmation_code_wrong_attempts_count,
