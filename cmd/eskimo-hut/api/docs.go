7e8c59e543728fd3348b1a5c41d06f27e44da4dd
Cronus <105345303+ice-cronus@users.noreply.github.com>
2023-12-05 15:16:52 +0300

    handle potentiallyDuplicate flag from face auth for in review state (#80)

--- cmd/eskimo-hut/api/docs.go
+++ cmd/eskimo-hut/api/docs.go
@@ -1610,6 +1610,9 @@ const docTemplate = `{
                     "example": [
                         "2006-01-02T15:04:05Z"
                     ]
+                },
+                "potentiallyDuplicate": {
+                    "type": "boolean"
                 }
             }
         },
